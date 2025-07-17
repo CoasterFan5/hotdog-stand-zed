0eb4f48d4a693d59c35093b5020da574374cdb79
Dylan <56899612+CoasterFan5@users.noreply.github.com>
2025-07-17 11:27:34 -0400

    Add hotdog midnight

--- themeTest.ts
+++ themeTest.ts
@@ -1,5 +1,7 @@
-let item = 0;
+let item: number = 0;
 const item2 = 5;
+const isThisTrue = true;
+
 const res = item + item2;
 const func = (item: number, item2: string) => {
   const newNumber = parseFloat(item2) + item;
@@ -8,3 +10,24 @@ const func = (item: number, item2: string) => {
 
 const stringLiteral = "asgasgag";
 const secondString = `asf${func(0, "1")}`;
+
+class Dog {
+  private a = 0;
+  constructor() {
+    this.a = 1;
+  }
+
+  getA() {
+    return this.a;
+  }
+}
+
+const newString = `\n\/adpsigjsjg`;
+
+enum Rarity {
+  Common = 0,
+  Uncommon = 1,
+  Rare = 2,
+  Mythic = 3,
+  Legendary = 4,
+}
